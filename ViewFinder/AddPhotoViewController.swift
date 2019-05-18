e64bd6cd9224fd2913d5a000bc87a40e159b358d
Marnie Reid <marniejeanreid@gmail.com>
2019-05-17 23:10:00 -0600

    Partial Iteration 1

---
+++ ViewFinder/AddPhotoViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Marnie Reid on 5/17/19.
+//  Copyright © 2019 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+        var imagePicker = UIImagePickerController()
+
+        override func viewDidLoad(){
+            super.viewDidLoad()
+            imagePicker.delegate = self
+        }
+
+
+        @IBAction func cameraTapped(_ sender: Any) {
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+
+        
+
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
